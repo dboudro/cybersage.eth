048a7451a46f3b58c0b93854fedd299b25b82ccb
LtEmerald <lieutenantemerald@protonmail.com>
2020-10-24 23:00:09 -0700

    styling updates

--- js/index.js
+++ js/index.js
@@ -9,6 +9,10 @@ var app = new Vue({
     globaltaglist: ["test"],
     search: "",
     nfts: "",
+    hedra:"",
+    ghost:"",
+    urbit:"",
+    weather: ""
   
   },
   methods: {
@@ -42,11 +46,27 @@ var app = new Vue({
     this.$http.get('https://api.opensea.io/api/v1/assets/?owner=0x92b406851390541F445B7C697014133d4B5BfcE3&order_direction=desc&offset=0&limit=20').then(function (data) {
         return data.json()
        
+      
     }).then(function (data) {
-      this.nfts = data;
+      this.nfts = data.assets;
+       this.ghost = data.assets[0]
+       this.hedra = data.assets[3]
+       this.urbit = data.assets[8]
     })
+
+    this.$http.get('http://wttr.in/Phoenix.txt').then(function (weather) {
+      return weather
+  }).then(function (weather) {
+    this.weather = weather;
+  })
+   
+
+  
+
+    
 },
   computed: {
-    // generate a list of tags and their counts with counts over 10
+    filteredassets: function () {
+  }
   },
 });
